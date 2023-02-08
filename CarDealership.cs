7682a852ccaf00a2a581eb808aab7be9f223ba89
Emzzy241 <emzzyoluwole@gmail.com>
2023-02-08 20:10:11 +0100

    adding my C# Code file

---
+++ CarDealership.cs
@@ -0,0 +1,10 @@
+// THIS APPLICATION HAS THE FOLLOWING AIMS..... Although you can come up with your own aims too
+
+
+/*
+    Try adding additional static methods to give the Car class interesting(and possibly quirky) functionalities
+    1. Adding a functionality to lower cost of the cars by a percentage amount for a sales event
+    2. Calculating how well each Car would perform or do in the Dakar Rally(a racing event). YOU CAN CALCULATE THIS HOWEVER YOU WANT
+    Though you may want to add more fields to your Car
+    3. Calculating each Car's resale value by Year..... Same as above, you can calculate this however you want
+*/
