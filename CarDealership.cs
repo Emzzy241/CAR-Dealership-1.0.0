bac2eaca04de4082cd43d09fe1dff2ba7d58d154
Emzzy241 <emzzyoluwole@gmail.com>
2023-02-09 10:32:45 +0100

    updating code with more App FUnctionality

--- CarDealership.cs
+++ CarDealership.cs
@@ -17,12 +17,44 @@ class CarDealership{
         // after our Main entry point, le us now write our full-fledged Application
         Console.WriteLine("Welcome to the Ema's Car Dealership, What would you like to purchase TOday");
 
-        Console.WriteLine("Enter T for Tricycles, B for Bikes, C fo Cars, P for Planes");
 
         // the Discount section
         Console.WriteLine("Please note that our Company is giving out Discounts to all Customers");
         Console.WriteLine("Get 0.2% on Cars you Purchase Today, Get 0.7% for any Planes you Purchase Today");
         Console.WriteLine("On other Automotives, Get 1.0% on Bikes you Purchase TOoday, Get 0.99%on Tricycles you Purchase TOday");
+        
+        Console.WriteLine("Enter T for Tricycles, B for Bikes, C fo Cars, P for Planes");
 
+        string userAutomotive = Console.ReadLine();
+        // working on capitalizations i.e Upper or Lower cases
+        string realUserAuto = userAutomotive.ToUpper();
+
+        if(realUserAuto == "T"){
+            Console.WriteLine("You Picked Tricycles, here are the ones we have:");
+            Console.WriteLine("Free-way Tricycle, Costs: ₦1,400,000");
+            Console.WriteLine("Ledger Tricycle, Costs: ₦1,100,000");
+            Console.WriteLine("Napep Tricycle, Costs: ₦1,200,000");
+        }
+        else if (realUserAuto == "B")
+        {
+            Console.WriteLine("You Picked Tricycles, here are the ones we have:");
+            Console.WriteLine("Zana Bike, Costs: ₦300,000");
+            Console.WriteLine("Suzuki Bike, Costs: ₦300,000");
+            Console.WriteLine("Baja Bike, Costs: ₦400,000");   
+        }
+        else if (realUserAuto == "C")
+        {
+            Console.WriteLine("You Picked Cars, here are the ones we have:");
+            Console.WriteLine("Honda Crosstour, Costs: ₦7,400,000");
+            Console.WriteLine("Lexus-350, Costs: ₦10,100,000");
+            Console.WriteLine("Toyota Camry , Costs: ₦3,200,000");
+        }
+        else if (realUserAuto == "P")
+        {
+            Console.WriteLine("You Picked Planes, here are the ones we have:");
+            Console.WriteLine("Air144, Costs: ₦290,400,000");
+            Console.WriteLine("Gulf-stream Jet, Costs: ₦900,100,000");
+            Console.WriteLine("G-5 Aerodynamo, Costs: ₦800,200,000");
+        }
     }
 }
